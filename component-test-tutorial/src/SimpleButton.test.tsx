9f963a6c32cade49b240d5d7fbd0d0800f722692
michi100 <a188ddwikou@gmail.com>
2024-12-15 16:35:23 +0900

    Reactコンポーネントをテスト.

---
+++ component-test-tutorial/src/SimpleButton.test.tsx
@@ -0,0 +1,20 @@
+// import { render, screen } from '@testing-library/react';
+// import { SimpleButton } from './SimpleButton';
+// import userEvent from '@testing-library/user-event';
+
+// test('ボタンをクリックするとON/OFFの表示が切り替わる', async () => {
+//   const user = userEvent.setup();
+//   render(<SimpleButton />);
+//   const simpleButton = screen.getByRole('button');
+//   expect(simpleButton).toHaveTextContent('OFF');
+//   await user.click(simpleButton);
+//   expect(simpleButton).toHaveTextContent('ON');
+// });
+
+import { render } from '@testing-library/react';
+import { SimpleButton } from './SimpleButton';
+
+test('初回レンダリング後すぐはOFFと表示されている.', () => {
+  const view = render(<SimpleButton />);
+  expect(view.container).toMatchSnapshot();
+});
